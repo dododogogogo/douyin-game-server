2ddc5a571fb88fd1995907a3a3a63bb697a25676
dododogogogo <loyzic@126.com>
2026-08-09 23:16:25 +0800

    init: spring boot

---
+++ src/main/java/com/example/douyin_game_ltqd/DouyinGameLtqdApplication.java
@@ -0,0 +1,13 @@
+package com.example.douyin_game_ltqd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DouyinGameLtqdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DouyinGameLtqdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/douyin_game_ltqd/DouyinGameLtqdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.douyin_game_ltqd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DouyinGameLtqdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
